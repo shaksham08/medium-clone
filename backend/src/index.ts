3388d5489347105bd46488be04703dab6b23dee7
shaksham08 <shakshamsinha@outlook.com>
2024-12-17 00:52:47 +0530

    init: Initialised the backend project with simple routes and prisma setup with postgres sql

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const app = new Hono();
+
+app.post("/api/v1/signup", (c) => {
+  const prisma = new PrismaClient().$extends(withAccelerate());
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
