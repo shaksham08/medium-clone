89bf6697a73b9b62b9eef78ec2eb4d906f0ecbe0
shaksham08 <shakshamsinha@outlook.com>
2024-12-17 12:29:24 +0530

    dev:Added changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,30 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: { DATABASE_URL: string; JWT_SECRET: string };
+}>();
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient().$extends(withAccelerate());
-  return c.text("Hello Hono!");
+  console.log(c.env);
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const secret = "mySecretKey";
+  const token = await sign({ user: user.id }, c.env.JWT_SECRET);
+  return c.json({
+    jwtToken: token,
+  });
 });
 
 app.post("/api/v1/signin", (c) => {
